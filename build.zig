8aba491bb15757320c129e05c2d6f072fddbc2c9
jcalabro <jamesrcalabro@gmail.com>
2025-03-25 10:56:57 -0400

    no indent

--- build.zig
+++ build.zig
@@ -10,28 +10,26 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    {
-        // Create a test step
-        const zoo_tests = b.addTest(.{
-            .name = "zoo-tests",
-            .root_module = zoo_module,
-        });
-        const run_zoo_tests = b.addRunArtifact(zoo_tests);
-        b.installArtifact(zoo_tests);
+    // Create a test step
+    const zoo_tests = b.addTest(.{
+        .name = "zoo-tests",
+        .root_module = zoo_module,
+    });
+    const run_zoo_tests = b.addRunArtifact(zoo_tests);
+    b.installArtifact(zoo_tests);
 
-        const race = b.option(bool, "race", "Build tests with the race detector enabled (default: false)") orelse false;
-        zoo_tests.root_module.sanitize_thread = race;
+    const race = b.option(bool, "race", "Build tests with the race detector enabled (default: false)") orelse false;
+    zoo_tests.root_module.sanitize_thread = race;
 
-        const test_step = b.step("test", "Run tests");
-        test_step.dependOn(&run_zoo_tests.step);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_zoo_tests.step);
 
-        // Create a docs step
-        const docs_dir = b.addInstallDirectory(.{
-            .source_dir = zoo_tests.getEmittedDocs(),
-            .install_dir = .prefix,
-            .install_subdir = "docs",
-        });
-        const docs_step = b.step("docs", "Generate documentation");
-        docs_step.dependOn(&docs_dir.step);
-    }
+    // Create a docs step
+    const docs_dir = b.addInstallDirectory(.{
+        .source_dir = zoo_tests.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    const docs_step = b.step("docs", "Generate documentation");
+    docs_step.dependOn(&docs_dir.step);
 }
