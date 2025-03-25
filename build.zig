6cfd99fabb7b09db9f60728e018ec7cbe0503cbe
jcalabro <jamesrcalabro@gmail.com>
2025-03-25 09:49:05 -0400

    build.zig fixes

--- build.zig
+++ build.zig
@@ -4,36 +4,34 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const zoo_module = b.createModule(.{
+    const zoo_module = b.addModule("zoo", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const zoo = b.addLibrary(.{
-        .name = "zoo",
-        .root_module = zoo_module,
-    });
-    b.installArtifact(zoo);
+    {
+        // Create a test step
+        const zoo_tests = b.addTest(.{
+            .name = "zoo-tests",
+            .root_module = zoo_module,
+        });
+        const run_zoo_tests = b.addRunArtifact(zoo_tests);
+        b.installArtifact(zoo_tests);
 
-    const zoo_tests = b.addTest(.{
-        .name = "zoo-tests",
-        .root_module = zoo_module,
-    });
-    const run_zoo_tests = b.addRunArtifact(zoo_tests);
-    b.installArtifact(zoo_tests);
+        const race = b.option(bool, "race", "Build tests with the race detector enabled (default: false)") orelse false;
+        zoo_tests.root_module.sanitize_thread = race;
 
-    const race = b.option(bool, "race", "Build tests with the race detector enabled (default: false)") orelse false;
-    zoo_tests.root_module.sanitize_thread = race;
+        const test_step = b.step("test", "Run tests");
+        test_step.dependOn(&run_zoo_tests.step);
 
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_zoo_tests.step);
-
-    const docs_dir = b.addInstallDirectory(.{
-        .source_dir = zoo.getEmittedDocs(),
-        .install_dir = .prefix,
-        .install_subdir = "docs",
-    });
-    const docs_step = b.step("docs", "Generate documentation");
-    docs_step.dependOn(&docs_dir.step);
+        // Create a docs step
+        const docs_dir = b.addInstallDirectory(.{
+            .source_dir = zoo_tests.getEmittedDocs(),
+            .install_dir = .prefix,
+            .install_subdir = "docs",
+        });
+        const docs_step = b.step("docs", "Generate documentation");
+        docs_step.dependOn(&docs_dir.step);
+    }
 }
